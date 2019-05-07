ed1c1a292b41d94c9181a2e1fa8d9a204c640945
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-07 11:41:00 +0300

    Views and Controllers

---
+++ controllers/products.js
@@ -0,0 +1,24 @@
+const products = [];
+
+exports.getAddProduct = (req, res) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product'
+  });
+};
+
+exports.postAddProduct = (req, res) => {
+  products.push({title: req.body.title});
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res) => {
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+};
