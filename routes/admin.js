8089fddc8cdd47f984c9ef5e10b1efd8e74fd7ac
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-07 09:47:37 +0300

    Pug layout

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    docTitle: 'Add Product',
+    path: '/admin/add-product'
+  });
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const path = require('path');
 
-const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  
+  res.render('shop', {
+    prods: products,
+    docTitle: 'Shop',
+    path: '/'
+  });
 });
 
 module.exports = router;
