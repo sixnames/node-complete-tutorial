08aab91842aaa4b672e30c661ddc171cc23554a3
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-07 08:07:01 +0300

    new styles

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,20 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res) => {
-  console.log(req.body);
+  
+  products.push({title: req.body.title});
+  
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
