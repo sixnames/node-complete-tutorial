5691c29855cc5c8a1e5394e42ca70ef3a5dd566e
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-09 09:45:25 +0300

    MongoDB connection

--- util/database.js
+++ util/database.js
@@ -1,9 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'shimla11', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://admin:rBSBbD9uhqVC3!b@cluster0-nb3qo.mongodb.net/test?retryWrites=true')
+    .then((result) => {
+      console.log('Connected');
+      callback(result);
+    })
+    .catch((err) => console.log(err));
+};
 
-
-module.exports = sequelize;
+module.exports = mongoConnect;
