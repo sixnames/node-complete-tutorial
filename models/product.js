f5b5b34e67a6a9b1143768edbf6cbd6fe6d4c7e6
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-09 12:07:00 +0300

    Fetch product and products

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -12,6 +13,26 @@ class Product {
     const db = getDb();
     return db.collection('products').insertOne(this)
       .then(result => console.log(result))
+      .catch(err => console.log(err))
+  }
+  
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products').find().toArray()
+      .then(products => {
+        return products;
+      })
+      .catch(err => console.log(err))
+  }
+  
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .find({_id: new mongodb.ObjectID(prodId)})
+      .next()
+      .then(product => {
+        return product;
+      })
       .catch(err => console.log(err))
   }
 }
