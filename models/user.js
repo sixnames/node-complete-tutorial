57a61ce5854adaee54af56c2ad7d5b4dbf78d44e
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-09 20:02:34 +0300

    Orders

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -18,6 +17,40 @@ const userSchema = new Schema({
     }]
   }
 });
+
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+  
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  
+  this.cart = { items: updatedCartItems };
+  return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  this.cart.items = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  return this.save();
+};
+
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
 
